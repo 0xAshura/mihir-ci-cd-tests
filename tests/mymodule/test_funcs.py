33d6187521a0a6fb962d55af74782c55bc4df047
Mihir Limbad <mihir.limbad@dcmail.ca>
2025-03-12 01:52:01 -0400

    Initial commit

--- tests/mymodule/test_funcs.py
+++ tests/mymodule/test_funcs.py
@@ -19,3 +19,8 @@ def test_multiply():
 @pytest.mark.difficult_operation
 def test_divide():
     assert divide(56, 8) == 7
+
+@pytest.mark.easy_operation
+def test_add_student_id():
+    # New test case using the last two digits of the student ID as the expected output
+    assert add(42, 42) == 84  # Replace 42, 42 with appropriate values if needed
